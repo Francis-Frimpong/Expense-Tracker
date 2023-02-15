f8afb5f9b6fadf21a67c922f535e869280309e7d
Francis-Frimpong <brakay1136@gmail.com>
2023-02-15 10:32:29 +0000

    clean up code

--- app.js
+++ app.js
@@ -9,34 +9,39 @@ let nameField = document.querySelector("#name");
 let amountField = document.querySelector("#amount");
 let dateField = document.querySelector("#date");
 
-// let value = {
-//   nameField: nameField.value,
-//   dateField: dateField.value,
-//   amountField: amountField.value,
-//};
-
-// let list = [];
-// list.push(value);
-// console.log(list);
 //Creating a clickable button functionality
-addButton.addEventListener("click", () => {
-  tableList();
+addButton.addEventListener("click", (e) => {
+  e.preventDefault(formField);
+  value.tableList();
+  clearField();
 });
 
-// cut.addEventListener("click", () => {
-//   //   deleteList();
-//   console.log("clicked");
-// });
+let deleteBtn = document.querySelector(".delete");
+let tBody = document.querySelector(".list-item");
 
-function tableList() {
-  displayData.innerHTML += `
-        <tr class = "data">
-            <td>${nameField.value}</td>
-            <td>${dateField.value}</td>
-            <td>${amountField.value}</td>
-            
-        </tr>
-            
-        <br/>
-        `;
-}
+let list = [];
+
+let value = {
+  nameField: nameField.value,
+  dateField: dateField.value,
+  amountField: +amountField.value,
+  tableList: function () {
+    displayData.innerHTML += `
+     <tr class = "data">
+                 <td>${value.nameField}</td>
+                 <td>${value.dateField}</td>
+                 <td>${value.amountField}</td>
+                <td class="del-btn"><button class="delete">delete</button></td>
+
+
+             </tr>
+
+             <br/>
+             `;
+    list.push(value);
+
+    console.log(list);
+  },
+};
+
+let clearField = () => formField.reset();
