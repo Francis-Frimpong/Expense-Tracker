e6dadd45542ac1554af975cf1b9337e527f0732a
Francis <brakay1136@gmail.com>
2024-09-23 08:44:22 -0700

    refactored JS code

--- app.js
+++ app.js
@@ -1,47 +1,56 @@
-const formField = document.querySelector("#form-field");
 const addButton = document.querySelector(".add-btn");
 const displayMessage = document.querySelector(".display-message");
-let displayData = document.querySelector(".display-data");
-let cut = document.querySelector(".delete-btn");
-let data = document.querySelector(".data");
 
-let nameField = document.querySelector("#name");
-let amountField = document.querySelector("#amount");
-let dateField = document.querySelector("#date");
+// let data = document.querySelector('.data');
+
+
+
+function add(e) {
+  e.preventDefault();
+  
+  let nameField = document.getElementById("name").value;
+  let amountField = document.getElementById("amount").value;
+  let dateField = document.getElementById("date").value;
+  
+  if(!nameField || !amountField || !dateField) return;
+
+  const tableRow = document.createElement('tr');
+  tableRow.classList.add('data')
+  
+  const td = document.createElement('td');
+  td.textContent = nameField;
+  const td2 = document.createElement('td');
+  td2.textContent = dateField;
+  const td3 = document.createElement('td');
+  td3.textContent = amountField;
+  
+  const td4 = document.createElement('td')
+  td4.classList.add('delete-btn');
+  const button = document.createElement('button')
+  button.classList.add('delete');
+  button.textContent = 'delete';
+
+  td4.appendChild(button);
+
+  tableRow.appendChild(td);
+  tableRow.appendChild(td2);
+  tableRow.appendChild(td3);
+  tableRow.appendChild(td4);
+
+  document.querySelector('.list-item').appendChild(tableRow);
+}
+
+
+function deleteItem (e) {
+  if (e.target.classList.contains('delete')){
+    e.target.parentElement.parentElement.remove()
+  }
+}
+
 
 //Creating a clickable button functionality
-addButton.addEventListener("click", (e) => {
-  e.preventDefault(formField);
-  value.tableList();
-  clearField();
-});
-
-let deleteBtn = document.querySelector(".delete");
-let tBody = document.querySelector(".list-item");
-
-let list = [];
-
-let value = {
-  nameField: nameField.value,
-  dateField: dateField.value,
-  amountField: +amountField.value,
-  tableList: function () {
-    displayData.innerHTML += `
-     <tr class = "data">
-                 <td>${value.nameField}</td>
-                 <td>${value.dateField}</td>
-                 <td>${value.amountField}</td>
-                <td class="del-btn"><button class="delete">delete</button></td>
-
-
-             </tr>
-
-             <br/>
-             `;
-    list.push(value);
-
-    console.log(list);
-  },
-};
+addButton.addEventListener("click", add);
 
 let clearField = () => formField.reset();
+
+document.querySelector('.list-item').addEventListener('click', deleteItem)
